5df6586a863c02cf8bdbecf11868e56b18ad8e61
Brahim <brahimaz@hotmail.com>
2026-06-08 17:45:49 +0200

    Add custom ML metrics

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,20 @@
 import joblib
 import pandas as pd
+from prometheus_client import Counter
 
 model = joblib.load("models/xgboost_model.pkl")
 scaler = joblib.load("models/scaler.pkl")
 threshold = joblib.load("models/threshold.pkl")
+
+prediction_counter = Counter(
+    "predictions_total",
+    "Total predictions made"
+)
+
+fraud_counter = Counter(
+    "fraud_predictions_total",
+    "Fraud predictions made"
+)
 
 def predict_transaction(data):
     df = pd.DataFrame([data])
@@ -11,7 +22,10 @@ def predict_transaction(data):
     df["Amount"] = scaler.transform(df[["Amount"]])
 
     proba = model.predict_proba(df)[:, 1][0]
+    prediction_counter.inc()
     pred = int(proba > threshold)
+    if pred == 1:
+        fraud_counter.inc()
 
     return {
         "fraud_probability": float(proba),
